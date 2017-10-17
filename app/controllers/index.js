60a27ad8b8cc50d21891a1e9d811bc81b634aa64
原野 <yuanye@yuanyedeMacBook-Pro.local>
2017-10-17 16:29:20 +0800

    second

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,7 @@ var Article = require('../models/articles');
 exports.index = function(req,res){
 	Article.find({}).sort({_id: -1}).limit(3).exec(function(err,articles){
 		res.render('index',{
-			title:'小楼兰的奔跑-首页',
+			title:'十里长廊旅游网',
 			articles:articles
 		})
 	})
