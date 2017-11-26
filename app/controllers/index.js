adacc34f9cbed4de9e82c90688a2d9682e853a99
tianye <838199892@qq.com>
2017-11-26 12:50:51 +0800

    十里改成百里

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,7 +5,7 @@ exports.index = function(req,res){
 	Article.find({}).sort({_id: -1}).limit(8).exec(function(err,articles){
 		Diary.find({}).sort({_id: -1}).limit(8).exec(function(err,diarys){
 			res.render('index',{
-				title:'十里长廊旅游网',
+				title:'百里长廊旅游网',
 				articles:articles,
 				diarys: diarys
 			})
